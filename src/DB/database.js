5ebdbacbbde2c08462859d2ccf00c321b55bb6a6
Abdurazaq333 <irazaqr986@gmail.com>
2024-07-30 13:39:51 +0500

    database are connected

---
+++ src/DB/database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
